57634ff12da81d97ae202a0e8be81af5ccf3ef82
Paul Arterburn <paul.arterburn@brandfolder.com>
2014-03-16 17:39:49 -0600

    Fix for ajax call

--- app/assets/javascripts/raplet.js
+++ app/assets/javascripts/raplet.js
@@ -23,22 +23,25 @@ $( "#addUser" ).submit(function( event ) {
     email = [$form.find( "input[name='email']" ).val()],
     name = $form.find( "input[name='email']" ).val(),
     url = $form.attr( "action" );
- 
-  var posting = $.post( url, { email: email, name: name, visible_to: 0,  } );
+
   $("#addUser").fadeOut('fast');
-  var content = '<center><img src="https://pipedrive.herokuapp.com/spinner.gif" height="20" width="20"></center>';
+  var content = '<center><img src="https://pipedrive.herokuapp.com/spinner.gif" height="20" width="20" style="margin-top:5px;"></center>';
   $( "#result" ).empty().append( content );
 
-  // Put the results in a div
-  posting.done(function( data ) {
-    console.log(data);
-    if (typeof data["data"] === 'undefined') {
-      content = "Error while adding user";
-    } else {
-      var user_id = data["data"]["id"];
-      var name = data["data"]["name"];
-      content = '<ul class="memberships"><li class="membership expanded"><div class="object"><a class="membership-link" href="https://app.pipedrive.com/person/details/'+user_id+'" site_name="Pipedrive" target="_blank" title="View '+name+' on Pipedrive"><div class="icon"><img alt="Favicon" src="https://pipedrive.herokuapp.com/pipedrive_favicon.ico"></div>'+name+'</a></div></li></ul>';
-    }
-    $( "#result" ).empty().append( content );
-  });
+  $.ajax({
+        url: url,
+        type: "POST",
+        data: {email: email, name: name, visible_to: 0},
+        complete: function(data) {
+          console.log(data);
+          if (typeof data["data"] === 'undefined') {
+            content = "Error while adding user";
+          } else {
+            var user_id = data["data"]["id"];
+            var name = data["data"]["name"];
+            content = '<ul class="memberships"><li class="membership expanded"><div class="object"><a class="membership-link" href="https://app.pipedrive.com/person/details/'+user_id+'" site_name="Pipedrive" target="_blank" title="View '+name+' on Pipedrive"><div class="icon"><img alt="Favicon" src="https://pipedrive.herokuapp.com/pipedrive_favicon.ico"></div>'+name+'</a></div></li></ul>';
+          }
+          $( "#result" ).empty().append( content );
+        });
+
 });
