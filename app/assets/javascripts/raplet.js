4f4f0f15ce7c691bcc5e4c712d83ee3784b60c5d
Paul Arterburn <paul.arterburn@brandfolder.com>
2014-03-16 16:29:29 -0600

    Better style

---
+++ app/assets/javascripts/raplet.js
@@ -0,0 +1,15 @@
+$('.last_activity').mouseover(function() {
+  $('.last_activity_note').css("display","block");
+});
+
+$('.last_activity').mouseout(function() {
+  $('.last_activity_note').css("display","none");
+});
+
+$('.next_activity').mouseover(function() {
+  $('.next_activity_note').css("display","block");
+});
+
+$('.next_activity').mouseout(function() {
+  $('.next_activity_note').css("display","none");
+});
